4f520bcafcd393aa2b76945fc2c782920fae252e
Christian <71178988+christianjokhaji@users.noreply.github.com>
2022-04-18 13:54:09 +0200

    Interactive image updated.

--- resources/javascript/scripts.js
+++ resources/javascript/scripts.js
@@ -1,6 +1,6 @@
 const changeImage = () => {
 	if (document.getElementById("Armenia-Ball").src.indexOf("resources/images/armeniaball.png") != -1)  {
-		document.getElementById("Armenia-Ball").src = "resources/images/we-are-our-mountains.jpg";
+		document.getElementById("Armenia-Ball").src = "resources/images/assyriaball.png";
 	}
 	else {
 		document.getElementById("Armenia-Ball").src = "resources/images/armeniaball.png";
